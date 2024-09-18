b608bbfec96a527757dd311124aa4ad45d77f999
Kelison <kls_kelison@hotmail.com>
2024-09-18 02:00:21 -0300

    desafio-dio

---
+++ Models/Reserva.cs
@@ -0,0 +1,57 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            //Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+
+            if (hospedes.Count<=Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                //Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Ocorreu uma exceção. O numero de hospedes é maior que a capacidade do quarto");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            //Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Retorna o valor da diária
+            decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.9M;
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new (nome: "Hóspede 1");
+Pessoa p2 = new (nome: "Hóspede 2");
+Pessoa p3 = new (nome: "hospede 3");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+hospedes.Add(p3);
+
+// Cria a suíte
+Suite suite = new (tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new (diasReservados: 9);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
